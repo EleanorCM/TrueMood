f8668e4fa74549e4a47c0f13702483d15bd392ec
Eleanor CM <97481864+EleanorCM@users.noreply.github.com>
2024-03-14 23:24:45 +0000

    Moved to separate repo - first commit

---
+++ controllers/controller.js
@@ -0,0 +1,396 @@
+const axios = require("axios");
+const path = require("path");
+const {validationResult} = require('express-validator');
+const session = require("express-session");
+const { time } = require("console");
+
+// TODO SET of contextual triggers (separate triggers table)
+
+// TODO: Flexbox
+
+
+exports.getLanding = async (req, res) => {
+  if (req.session.isloggedin) {
+    res.redirect('/createsnapshot');
+  }
+  const response = await axios.get(
+    "https://zenquotes.io/api/random/[your_key]"
+  );
+
+  try {
+    if (response.status === 200) {
+      console.log(response.data);
+      res.status(200).render("landingpage", { data: response.data, error:null });
+    } else {
+      console.log(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getCreateSnapshot = async (req, res) => {
+  const u_id = req.session.userid;
+  const response = await axios.get(`http://localhost:3002?userid=${u_id}`);
+  const rows = response.data.result;
+
+  try {
+    if (response.status === 200) {
+      console.log(`Response status code: ${response.status}`);
+      res.status(200).render("createsnapshot", { data: rows });
+    } else {
+      console.log(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error fetching snapshots from the API:", error);
+    res.status(500).render("error", {
+      message: "An error occurred while fetching snapshots",
+    });
+  }
+};
+
+exports.postCreateSnapshot = async (req, res) => {
+  const user_id = req.session.userid;
+  const {
+    datetime,
+    comments,
+    happiness_rating,
+    surprise_rating,
+    contempt_rating,
+    sadness_rating,
+    fear_rating,
+    disgust_rating,
+    anger_rating,
+  } = req.body;
+
+  const vals = [
+    user_id,
+    comments,
+    happiness_rating,
+    surprise_rating,
+    contempt_rating,
+    sadness_rating,
+    fear_rating,
+    disgust_rating,
+    anger_rating,
+  ];
+
+  const response = await axios.post("http://localhost:3002/", {
+    user_id,
+    comments,
+    happiness_rating,
+    surprise_rating,
+    contempt_rating,
+    sadness_rating,
+    fear_rating,
+    disgust_rating,
+    anger_rating,
+  });
+
+  try {
+    if (response.status === 201) {
+      console.log("RESPONSE BODY:");
+      console.log(response);
+      res.cookie("comments", comments);
+      console.log(`Response status code: ${response.status}`);
+      res.redirect("/createsnapshot");
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error creating snapshot:", error);
+    res.status(501).render("error", {
+      message: "An error occurred while creating the snapshot",
+    });
+  }
+};
+
+exports.getEditSnapshot = async (req, res) => {
+  const { id } = req.params;
+  const response = await axios.get(`http://localhost:3002/editsnapshot/${id}`);
+  const rows = response.data.result;
+  console.log(rows);
+
+  try {
+    if (response.status === 200) {
+      console.log(`Response status code: ${response.status}`);
+      res.status(200).render("editsnapshot", { result: rows });
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error fetching snapshot:", error);
+    res.status(500).render("error", {
+      message: "An error occurred while fetching snapshot with ID " + id,
+    });
+  }
+};
+
+exports.postEditSnapshot = async (req, res) => {
+  console.log(req.session);
+
+  const {
+    datetime,
+    comments,
+    happiness_rating,
+    surprise_rating,
+    contempt_rating,
+    sadness_rating,
+    fear_rating,
+    disgust_rating,
+    anger_rating,
+  } = req.body;
+
+  const vals = [
+    comments,
+    happiness_rating,
+    surprise_rating,
+    contempt_rating,
+    sadness_rating,
+    fear_rating,
+    disgust_rating,
+    anger_rating,
+  ];
+
+  const { id } = req.params;
+  const response = await axios.post(
+    `http://localhost:3002/editsnapshot/${id}`,
+    {
+      comments,
+      happiness_rating,
+      surprise_rating,
+      contempt_rating,
+      sadness_rating,
+      fear_rating,
+      disgust_rating,
+      anger_rating,
+    }
+  );
+
+  try {
+    if (response.status === 201) {
+      console.log(`Response status code: ${response.status}`);
+      res.redirect("/createsnapshot");
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error updating snapshot:", error);
+    res.status(501).render("error", {
+      message: "An error occurred while updating snapshot with ID " + id,
+    });
+  }
+};
+
+exports.getDeleteSnapshot = async (req, res) => {
+  const { id } = req.params;
+  const response = await axios.get(
+    `http://localhost:3002/deletesnapshot/${id}`
+  );
+
+  try {
+    if (response.status <= 300) {
+      console.log(`Response status code: ${response.status}`);
+      res.redirect("/createsnapshot");
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+exports.getInsights = async (req, res) => {
+  const filter = req.query.emotion;
+  const userid = req.session.userid;
+  const response = await axios.get(`http://localhost:3002/insights?userid=${userid}`);
+  const rows = response.data.result;
+
+
+  try {
+    if (response.status === 200) {
+      console.log(`Response status code: ${response.status}`);
+      let ratings = [];
+      let timestamps = [];
+
+      rows.forEach(row => {
+        const rating = row[filter];
+        ratings.push(rating);
+
+        dateString = row.datetime;
+        const dateRegex = /^(\d{4}-\d{2}-\d{2})T/;
+        const matchResult = dateString.match(dateRegex);
+        if (matchResult) {
+          const extractedDate = matchResult[1];
+          console.log("Extracted date:", extractedDate);
+          timestamps.push(extractedDate);
+        } else {
+          console.log("No date found.");
+        }
+        timestamps.reverse();
+        ratings.reverse();
+        
+      });
+      console.log("RATINGS");
+      console.log(ratings);
+      console.log("TIMESTAMPS");
+      console.log(timestamps);
+      res.render("insights", { rows: rows, userid: userid, ratings: ratings, timestamps: timestamps });
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+
+};
+
+exports.getFilterSnapshots = async (req, res) => {
+  const filter = req.query.emotion;
+  const userid = req.session.userid;
+  res.cookie("emotion", filter, { maxAge: 60000 });
+  const response = await axios.get(`http://localhost:3002/insights/filter?userid=${userid}&filter=${filter}`);
+  const rows = response.data.result;
+  console.log("FILTER INSIGHTS ROWS: ");
+  console.log(rows);
+  res.cookie("emotion", filter, { maxAge: 60000 });
+
+  try {
+    if (response.status === 200) {
+      console.log(`Response status code: ${response.status}`);
+      
+      let ratings = [];
+      let timestamps = [];
+
+      rows.forEach(row => {
+        const rating = row[filter];
+        ratings.push(rating);
+
+        dateString = row.datetime;
+        const dateRegex = /^(\d{4}-\d{2}-\d{2})T/;
+        const matchResult = dateString.match(dateRegex);
+        if (matchResult) {
+          const extractedDate = matchResult[1];
+          console.log("Extracted date:", extractedDate);
+          timestamps.push(extractedDate);
+        } else {
+          console.log("No date found.");
+        }
+        ratings.reverse();
+        timestamps.reverse();
+
+        
+      });
+      console.log("RATINGS");
+      console.log(ratings);
+      console.log("TIMESTAMPS");
+      console.log(timestamps);
+
+      res.render("insights", { rows: rows, userid:userid, filter:response.filter, ratings: ratings, timestamps:timestamps });
+      console.log(req.session);
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+    console.log(rows);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+exports.postLogin = async (req, res) => {
+const errors = validationResult(req);
+console.log(errors.array);
+if (!errors.isEmpty()) {
+  return res.status(422).render('landingpage', (errors.array()[0].msg))
+}
+
+  const { email, userpass } = req.body;
+  const vals = [email, userpass];
+  console.log(vals);
+
+  const response = await axios.post("http://localhost:3002/login", {
+    email: email,
+    userpass: userpass
+  });
+  const rows = response.data.result;
+  const userid = rows.id;
+
+  try {
+    if (response.status === 201) {
+      console.log(`Response status code: ${response.status}`);
+      console.log("RESPONSE " + response);
+      console.log("ROWS" + rows);
+      const numrows = rows.length;
+      console.log("numrows: " + numrows);
+
+      if (numrows > 0) {
+        // Adding properties to session
+        const session = req.session;
+        session.isloggedin = true;
+        session.email = email;
+        session.userid = userid;
+        let original_route = session.route;
+        console.log(`postLogin: orig_route: ${original_route}`);
+        console.log(session);
+        if (!original_route) {
+          original_route = "/createsnapshot";
+        }
+        if (original_route != "/login") {
+          res.redirect(`${original_route}`);
+        }
+      } else {
+        res.redirect("/");
+      }
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    console.error("Error logging in:", error);
+    res.status(501).render("error", {
+      message:
+        "An error occurred while trying to log in with the supplied credentials",
+    });
+  }
+};
+
+exports.getSignUp = async (req,res) => {
+  try {
+      res.status(200).render("signup");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.postSignUp = async (req,res) => {
+  const {newuserfirstname, newuserlastname, newuseremail, newuserpassword} = req.body;
+
+  const response = await axios.post('http://localhost:3002/signup' , {
+    newuserfirstname:newuserfirstname, 
+    newuserlastname:newuserlastname, 
+    newuseremail:newuseremail, 
+    newuserpassword:newuserpassword})
+
+  rows = response.data.result;
+
+  try {
+    if(response.status === 201) {
+      console.log(`Response status code: ${response.status}`);
+      res.redirect('/createsnapshot');
+    } else {
+      console.error(`Response status code: ${response.status}`);
+    }
+  } catch (error) {
+    
+  }
+
+}
+
+exports.getLogout = (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+};
+
+exports.get404 = (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "../html/404.html"));
+};
---
+++ routes/myroutes.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const controller = require("../controllers/controller");
+const {isAuth} = require('./../middleware/auth');
+const {check} = require('express-validator');
+
+const router = express.Router();
+
+router.get("/", controller.getLanding);
+router.get("/editsnapshot/:id", isAuth, controller.getEditSnapshot);
+router.get('/deletesnapshot/:id', isAuth, controller.getDeleteSnapshot);
+router.get('/insights', isAuth, controller.getInsights);
+router.get('/insightsfilter', isAuth, controller.getFilterSnapshots);
+router.get('/login', controller.postLogin);
+router.get('/logout', controller.getLogout)
+router.get('/createsnapshot', isAuth, controller.getCreateSnapshot);
+router.get('/signup', controller.getSignUp);
+router.get("*", controller.get404);
+
+router.post("/createsnapshot", isAuth, controller.postCreateSnapshot);
+router.post("/editsnapshot/:id", isAuth, controller.postEditSnapshot);
+router.post('/login',
+    check('email')
+    .isEmail()
+    .withMessage('Invalid email address'),
+     controller.postLogin);
+router.post('/signup', 
+    check('email')
+    .isEmail()
+    .withMessage('Invalid email address')
+    ,controller.postSignUp);
+router.post("*", controller.get404);
+
+module.exports = router;
